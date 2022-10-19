72c32450c2181bbf4553e9df1037fb18f9870018
Felix Ondullah <felixondullah@gmail.com>
2022-10-19 13:40:32 +0300

    Completed

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+class Dog
+    def bark
+        puts "Woof!"
+    end
+    def sit
+       puts  "The Dog is sitting"
+    end
+end
+fido = Dog.new
+fido.bark
+snoppy = Dog.new
+snoppy.bark
+fido.sit
